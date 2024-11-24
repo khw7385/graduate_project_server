932a2399c049ea48a86e7dd59e356a3c6471ef06
hyunwon <khw73850@gmail.com>
2024-11-24 13:45:24 +0900

    init

---
+++ src/test/java/me/khw7385/graduate_project/GraduateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khw7385.graduate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
