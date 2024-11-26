6fae2422f889b5a567b74abfe0115f5f5a1b7482
hyunwon <khw73850@gmail.com>
2024-11-26 17:11:37 +0900

    feat: Query Plan Repository 구현

---
+++ src/main/java/me/khw7385/graduate_project/repository/QueryPlanRepository.java
@@ -0,0 +1,42 @@
+package me.khw7385.graduate_project.repository;
+
+import me.khw7385.graduate_project.domain.QueryPlan;
+import me.khw7385.graduate_project.domain.UserQueryPlanInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class QueryPlanRepository {
+    private final Map<String, UserQueryPlanInfo> queryInfoMap = new HashMap<>();
+    private static final QueryPlanRepository INSTANCE = new QueryPlanRepository();
+
+    public static QueryPlanRepository getInstance(){
+        return INSTANCE;
+    }
+
+    public void initUserQueryInfo(String userId){
+        queryInfoMap.put(userId, new UserQueryPlanInfo());
+    }
+
+    public void addQueryPlan(String userId, QueryPlan queryPlan){
+        UserQueryPlanInfo userQueryPlanInfo = queryInfoMap.get(userId);
+        userQueryPlanInfo.addQueryPlan(queryPlan);
+    }
+
+    public UserQueryPlanInfo findByUserId(String userId){
+        return queryInfoMap.get(userId);
+    }
+
+    public void setOptimalQueryPlan(String userId, QueryPlan queryPlan){
+        queryPlan.setDistance(0 ,0 ,0);
+        UserQueryPlanInfo userQueryPlanInfo = queryInfoMap.get(userId);
+        userQueryPlanInfo.setOptimalQueryPlan(queryPlan);
+        userQueryPlanInfo.setMinCost(queryPlan.getCost());
+    }
+
+    public void setMaxCost(String userId, double maxValue){
+        UserQueryPlanInfo userQueryPlanInfo = queryInfoMap.get(userId);
+        userQueryPlanInfo.setMaxCost(maxValue);
+    }
+}
